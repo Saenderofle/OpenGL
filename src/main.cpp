7cb1efd1fd54d7b7af5c3c98b004753d5646c5e0
Daniel Sharshunov <158561450+Saenderofle@users.noreply.github.com>
2026-06-04 16:37:14 +0300

    HW_2
    rectangle creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,152 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+
+const char* vertexShaderSource = R"glsl(
+    #version 330 core
+    layout (location = 0) in vec3 aPos;   // Позиція вершини
+    layout (location = 1) in vec3 aColor; // Колір вершини
+
+    out vec3 ourColor; // Передаємо колір у Fragment Shader
+
+    void main() {
+        gl_Position = vec4(aPos, 1.0);
+        ourColor = aColor; // Інтерполюється автоматично між вершинами
+    }
+)glsl";
+
+
+const char* fragmentShaderSource = R"glsl(
+    #version 330 core
+    in vec3 ourColor;
+    out vec4 FragColor;
+
+    void main() {
+        FragColor = vec4(ourColor, 1.0f);
+    }
+)glsl";
 
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
+    // Ініціалізація GLFW
+    if (!glfwInit()) return -1;
 
-    const auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Interpolated Rectangle", nullptr, nullptr);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        return -1;
     }
 
+    // -------------------------------------------------------------------------
+    // Компіляція Шейдерів
+    // -------------------------------------------------------------------------
+    // Vertex Shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    // Шейдерна програма
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // -------------------------------------------------------------------------
+    // Налаштування геометрії (Вершини та Індекси)
+    // -------------------------------------------------------------------------
+    // Координати (X, Y, Z) та Кольори (R, G, B) для 4 вершин прямокутника
+    float vertices[] = {
+        // Позиції            // Кольори
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f, // Вершина 1: Верхня права (Червона)
+         0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 0.0f, // Вершина 2: Нижня права  (Жовта)
+        -0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f, // Вершина 3: Нижня ліва   (Зелена)
+        -0.5f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f  // Вершина 4: Верхня ліва  (Синя)
+    };
+
+    // Порядок з'єднання вершин для утворення двох трикутників (Прямокутника)
+    unsigned int indices[] = {
+        0, 1, 3, // Перший трикутник (Верхній-правий)
+        1, 2, 3  // Другий трикутник (Нижній-лівий)
+    };
+
+    unsigned int VAO, VBO, EBO;
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(0);
+
+    // -------------------------------------------------------------------------
+    // Головний цикл рендерингу
+    // -------------------------------------------------------------------------
+    while (!glfwWindowShouldClose(window)) {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, true);
+
+
+        glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Малювання прямокутника
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
+
+    glfwTerminate();
+    return 0;
 }
